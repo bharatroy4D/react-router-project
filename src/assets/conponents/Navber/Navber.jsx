00e1eb172c612727303e43c46043f8ceadf5ac98
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-28 14:59:18 +0600

    card into animation added

--- src/assets/conponents/Navber/Navber.jsx
+++ src/assets/conponents/Navber/Navber.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const Navber = () => {
     return (
         <div className='flex justify-center bg-gray-400 p-5 text-black gap-10 list-none'>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/section'>Section</Link>
-            <Link to='/content'>Content</Link>   
-            <Link to='/users'>users</Link>   
-            <Link to ='/posts'>posts</Link>
+            <Link className='hover:bg-blue-400 py-1 px-3 rounded-md' to='/'>Home</Link>
+            <Link className='hover:bg-blue-400 py-1 px-3 rounded-md' to='/about'>About</Link>
+            <Link className='hover:bg-blue-400 py-1 px-3 rounded-md' to='/section'>Section</Link>
+            <Link className='hover:bg-blue-400 py-1 px-3 rounded-md' to='/content'>Content</Link>   
+            <Link className='hover:bg-blue-400 py-1 px-3 rounded-md' to='/users'>users</Link>     
+            <Link className='hover:bg-blue-400 py-1 px-3 rounded-md' to ='/posts'>posts</Link>
                   
         </div>
     );
--- src/assets/conponents/Post/Post.jsx
+++ src/assets/conponents/Post/Post.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import { Button } from 'antd';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Post = ({ post }) => {
-    console.log(post)
     const { id, title, body } = post;
+
+    // Initialize AOS inside the component
+    useEffect(() => {
+        AOS.init({
+            duration: 1000, // Animation duration
+            easing: 'ease-in-out', // Smooth effect
+            once: true, // Ensures animation runs only once per scroll
+        });
+    }, []);
+
     return (
-        <div>
-            <div className='border border-blue-700 rounded-lg p-8'>
-                <h3>post id:{id}</h3>
-                <h3>post Title:{title}</h3>
-                <p>post body: {body}</p>
-                <Link className='text-blue-800' to={`/post/${id}`}>Post details</Link>
+        <div
+            data-aos="fade-right"
+            className='w-64 h-64 border border-blue-700 rounded-lg p-3 flex flex-col justify-between shadow-lg'
+        >
+            <div>
+                <h3 className='font-bold'>Post ID: {id}</h3>
+                <h3 className='font-semibold text-left'>Title: {title}</h3>
+                <p className='text-left overflow-hidden text-ellipsis line-clamp-3'>
+                    {body}
+                </p>
             </div>
+
+            <Link className='text-blue-800 font-medium' to={`/post/${id}`}>
+                Post details
+            </Link>
+
+            <Button type="dashed">
+                Solid
+            </Button>
         </div>
     );
 };
